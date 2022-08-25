7b2f907cda29418221eb68dcaa2362e0e08bfee0
Ozkan Kara <ozzyblackk73@gmail.comgit config --global user.email ozzyblackk73@gmail.com>
2022-08-24 17:03:20 -0700

    temperature check basic code is there in the repo

---
+++ src/test/java/class01/GitBasics.java
@@ -0,0 +1,9 @@
+package class01;
+
+public class GitBasics {
+    public static void main(String[] args) {
+        // write a code that checks the temperature afrom a website amd then
+        // prints out that it is a beautiful day or not
+        System.out.println("Today is a beautiful day");
+    }
+}
